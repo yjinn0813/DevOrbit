8dbfe7979ee8af22da03464dc653a2e9922031c9
yjinn0813 <yjinn0813@gmail.com>
2026-09-15 16:29:32 +0900

    feat: integrate developer insights and improve detail layout

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -5,6 +5,10 @@ import ProfileHeader from '../components/dashboard/ProfileHeader';
 import ActivityStats from '../components/dashboard/ActivityStats';
 import RepoStats from '../components/dashboard/RepoStats';
 import LanguageChart from '../components/dashboard/LanguageChart';
+import ContributionTrend from '../components/dashboard/ContributionTrend';
+import ActiveRepo from '../components/dashboard/ActiveRepo';
+import ContributionStreak from '../components/dashboard/ContributionStreak';
+import TierBadge from '../components/dashboard/TierBadge';
 import DetailSkeleton from '../components/dashboard/DetailSkeleton';
 import Error from './Error';
 import NotFound from './NotFound';
@@ -29,16 +33,33 @@ const Detail = () => {
   const user = data.data.user;
   
   return (
-    <div className="grid grid-cols-1 gap-5 mx-5 my-7 lg:grid-cols-2">
-      <div className="lg:col-span-2">
-        <ProfileHeader user={user} />
+    <div className="mx-5 my-7 flex flex-col gap-5">
+      {/* Profile + Tier */}
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-5">
+        <div className="md:col-span-3">
+          <ProfileHeader user={user} />
+        </div>
+
+        <div className="md:col-span-2">
+          <TierBadge />
+        </div>
       </div>
 
-      <ActivityStats user={user} />
-      <RepoStats user={user} />
+      {/* Dashboard */}
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+        <ActivityStats user={user} />
+        <RepoStats user={user} />
 
-      <div className="lg:col-span-2">
-        <LanguageChart user={user} />
+        <div className="md:col-span-2">
+          <LanguageChart user={user} />
+        </div>
+
+        <div className="md:col-span-2">
+          <ContributionTrend user={user} />
+        </div>
+
+        <ActiveRepo user={user} />
+        <ContributionStreak />
       </div>
     </div>
   );
